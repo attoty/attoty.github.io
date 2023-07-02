1d97c29d9c2825be4bc822645917850993b3dfb4
attoty <79607797+attoty@users.noreply.github.com>
2023-07-02 19:54:29 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ pass3 = str(random.randint(2, 9))
 pass4 = str(random.randint(2, 9))
 passcode = pass1 + pass2 + pass3 + pass4
 sword = random.choice(["shiny", "rusty"])
-
+score = 0
 
 def print_sleep(text, wait_time):
     print(text)
     time.sleep(wait_time)
-
 
 def intro():
     print_sleep("Welcome to the ancient Egyptian world, where mysteries await "
@@ -33,6 +32,8 @@ def intro():
     print_sleep("And he found a secret room", 1)
     print_sleep("Once you enter the room the door behind you locks", 2)
     print_sleep("you find three rooms in front of you ", 2)
+    print_sleep("in this game there is a scoring system", 1)
+    print_sleep("as well as win and lose conditions", 1)
     print_sleep("To start the game type in start", 1)
     intro_answer = ""
     while intro_answer not in ["start"]:
@@ -44,7 +45,7 @@ def intro():
 def play_again():
     choice = ''
     while choice not in ['y', 'n']:
-        choice = input("Would you like to play again? (y/n)")
+        choice = input("Would you like to play again? (y/n)\n")
         if choice == 'n':
             print_sleep("Thanks for playing!", 2)
             quit()
@@ -67,6 +68,8 @@ def play_again():
 
 def hall():
     global sword
+    global score
+    print_sleep("your score is " + str(score), 1)
     print_sleep("Choose where do you want to go", 2)
     print_sleep("room 1?", 1)
     print_sleep("room 2?", 1)
@@ -104,16 +107,18 @@ def hall():
 def room1():
     global lever1
     global room1_visited
+    global score
     room1_visited = True
     print_sleep("You enter the room you find a lever and next to it a papyrus", 2)
     print_sleep("you pick the papyrus and read it", 2)
     print_sleep(f"In the {pass1} night with {pass2} moons there is a secret", 2)
     lever1_choice = ""
     while lever1_choice not in ["y", "n"]:
-        lever1_choice = input("choose to open the lever or not (y/n)")
+        lever1_choice = input("choose to open the lever or not (y/n)\n")
     if lever1_choice == "y":
         print_sleep("You open the lever and nothing happens so you leave the room", 2)
         lever1 = 1
+        score += 10
         hall()
     elif lever1_choice == "n":
         print_sleep("You leave the room and go back to the hall", 2)
@@ -125,10 +130,12 @@ def room2():
     global lever2
     global lever3
     global sword
+    global score
     if room3_visited and sword == "rusty":
         print_sleep("now you can upgrade your sword", 2)
         print_sleep("you upgrade it and go back to the large room", 2)
         sword = "shiny"
+        score += 10
         hall2()
     else:
         print_sleep("You enter the room and find an Anubis statue, the god of "
@@ -143,9 +150,11 @@ def room2():
             if levers_choice == "1":
                 print_sleep("you pull the first lever and nothing happens", 2)
                 lever2 = 1
+                score +=  10
             elif levers_choice == "2":
                 print_sleep("you pull the second lever also nothing happens", 2)
                 lever3 = 1
+                score += 10
             if lever2 == 1 and lever3 ==1:
                 break 
         print_sleep("so you try to pull both at the same time and it finally works", 2)
@@ -159,9 +168,11 @@ def room2():
 
 def room3():
     global room3_visited
+    global score
     if lever1 == 1 and lever2 == 1 and  lever3 == 1:
         print_sleep("You find a door with a passcode, and beside it, a chest.", 2)
         print_sleep(f"You open the chest and find a {sword} sword.", 2)
+        score += 10
         print_sleep("You take the sword and check the door.", 2)
         print_sleep("It needs a password.", 1)
         print_sleep("HINT<papyrus and statue>", 2)
@@ -171,9 +182,11 @@ def room3():
             user_passcode = input("Type in the password: ")
             if user_passcode == passcode:
                 print_sleep("Correct password.", 2)
+                score  += 20
                 break
             elif user_passcode != passcode:
                 trials -= 1
+                score -= 5
                 if trials == 0:
                     print_sleep("you have been locked here forever", 2)
                     play_again()
@@ -187,23 +200,28 @@ def room3():
         hall()
 
 def hall2():
+    global score
+    print_sleep("your score is " + str(score), 1)
     print_sleep("Inside the room, you find a lake with crocodiles in it", 2)
     print_sleep("type 1 to swim in the lake", 2)
     print_sleep("type 2 to go back", 2)
     hall2_choice = ""
     while hall2_choice not in ["1", "2"]:
-        hall2_choice = input("what do you want to do?")
+        hall2_choice = input("what do you want to do?\n")
         if hall2_choice == "1":
             if sword == "rusty":
                 print_sleep("you swim and try to attack them with the rusty sword but fail and die", 2)
                 play_again()
             elif sword == "shiny":
                 print_sleep("you swim and attack them and go to the other side of the lake", 2)
+                score += 10
                 boss()
         elif hall2_choice == "2":
             hall()
 
 def boss():
+    global score
+    print_sleep("your score is " + str(score), 1)
     print_sleep("You continue and find a large door with the eye of horus on it", 2)
     print_sleep("You use the key on the door and it opens", 2)
     print_sleep("inside you find a mommy that is protecting the exit door", 2)
@@ -214,31 +232,38 @@ def boss():
     attack1 = ""
     mommy_health = 150
     while attack1 not in ["1", "2", "3"]:
-        attack1 = input ("choose number of attack")
+        attack1 = input ("choose number of attack\n")
         if attack1 == "1":
             mommy_health -= 70
+            score += 25
             print_sleep("mommy health = " + str(mommy_health), 2)
         elif attack1 == "2":
+            score += 15
             mommy_health -= 50
             print_sleep("mommy health = " + str(mommy_health), 2)
         elif attack1 == "3":
             mommy_health -= 60
+            score += 20
             print_sleep("mommy health = " + str(mommy_health), 2)
     print_sleep("Nice attack!", 1)
     print_sleep("but you still have more to go", 2)
+    print_sleep("your score is " + score, 1)
     print_sleep("Terror <1>", 1)
     print_sleep("Barrage <2>", 1)
     print_sleep("Fussilade <3>", 1)
     attack2 = ""
     while attack2 not in ["1", "2", "3"]:
-        attack2 = input ("choose number of attack")
+        attack2 = input ("choose number of attack\n")
         if attack2 == "1":
             mommy_health -= 80
+            score += 25
             print_sleep("mommy health = " + str(mommy_health), 2)
         elif attack2 == "2":
+            score += 20
             mommy_health -= 60
             print_sleep("mommy health = " + str(mommy_health), 2)
         elif attack2 == "3":
+            score += 10
             mommy_health -= 50
             print_sleep("mommy health = " + str(mommy_health), 2)
         if mommy_health < 0:
@@ -251,28 +276,32 @@ def boss():
             print_sleep("Ooze <3>", 1)
             attack3 = ""
             while attack3 not in ["1", "2", "3"]:
-                attack3 = input ("choose number of attack")
+                attack3 = input ("choose number of attack\n")
                 if attack3 == "1":
                     mommy_health -= 70
                     if mommy_health < 0:
                         mommy_health = 0
+                    score += 15
                     print_sleep("mommy health = " + str(mommy_health), 2)
                 elif attack3 == "2":
                     if mommy_health < 0:
                         mommy_health = 0
                     mommy_health -= 55
+                    score += 5
                     print_sleep("mommy health = " + str(mommy_health), 2)
                 elif attack3 == "3":
                     if mommy_health < 0:
                         mommy_health = 0
                     mommy_health -= 60
+                    score += 10
                     print_sleep("mommy health = " + str(mommy_health), 2)
     print_sleep("you finally kill him", 1) 
     End()
 def End():
     print_sleep("Using the key you have ", 1)
     print_sleep("You open the exit and escape", 2)
+    print_sleep("your final score is " + str(score), 1)
+    print_sleep("try again maybe you will get higher score", 1)
     play_again()
     
-sword = "rusty"
-hall()
+intro()
